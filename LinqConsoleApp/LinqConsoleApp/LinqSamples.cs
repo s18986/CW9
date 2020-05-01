8653a5fe7f0f21aacbb6f7ab81c0c42f67555be2
s18986 <s18986@users.noreply.github.com>
2020-05-01 12:40:43 +0200

    dodanie wszystkich poza 10( nigdy nie ogarnialem union) w miare mozliwosci operujac na lamdach

---
+++ LinqConsoleApp/LinqConsoleApp/LinqSamples.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LinqConsoleApp
+{
+    public class LinqSamples
+    {
+        public static IEnumerable<Emp> Emps { get; set; }
+        public static IEnumerable<Dept> Depts { get; set; }
+
+        public LinqSamples()
+        {
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            var empsCol = new List<Emp>();
+            var deptsCol = new List<Dept>();
+
+            #region Load depts
+            var d1 = new Dept
+            {
+                Deptno = 1,
+                Dname = "Research",
+                Loc = "Warsaw"
+            };
+
+            var d2 = new Dept
+            {
+                Deptno = 2,
+                Dname = "Human Resources",
+                Loc = "New York"
+            };
+
+            var d3 = new Dept
+            {
+                Deptno = 3,
+                Dname = "IT",
+                Loc = "Los Angeles"
+            };
+
+            deptsCol.Add(d1);
+            deptsCol.Add(d2);
+            deptsCol.Add(d3);
+            Depts = deptsCol;
+            #endregion
+
+            #region Load emps
+            var e1 = new Emp
+            {
+                Deptno = 1,
+                Empno = 1,
+                Ename = "Jan Kowalski",
+                HireDate = DateTime.Now.AddMonths(-5),
+                Job = "Backend programmer",
+                Mgr = null,
+                Salary = 2000
+            };
+
+            var e2 = new Emp
+            {
+                Deptno = 1,
+                Empno = 20,
+                Ename = "Anna Malewska",
+                HireDate = DateTime.Now.AddMonths(-7),
+                Job = "Frontend programmer",
+                Mgr = e1,
+                Salary = 4000
+            };
+
+            var e3 = new Emp
+            {
+                Deptno = 1,
+                Empno = 2,
+                Ename = "Marcin Korewski",
+                HireDate = DateTime.Now.AddMonths(-3),
+                Job = "Frontend programmer",
+                Mgr = null,
+                Salary = 5000
+            };
+
+            var e4 = new Emp
+            {
+                Deptno = 2,
+                Empno = 3,
+                Ename = "Paweł Latowski",
+                HireDate = DateTime.Now.AddMonths(-2),
+                Job = "Frontend programmer",
+                Mgr = e2,
+                Salary = 5500
+            };
+
+            var e5 = new Emp
+            {
+                Deptno = 2,
+                Empno = 4,
+                Ename = "Michał Kowalski",
+                HireDate = DateTime.Now.AddMonths(-2),
+                Job = "Backend programmer",
+                Mgr = e2,
+                Salary = 5500
+            };
+
+            var e6 = new Emp
+            {
+                Deptno = 2,
+                Empno = 5,
+                Ename = "Katarzyna Malewska",
+                HireDate = DateTime.Now.AddMonths(-3),
+                Job = "Manager",
+                Mgr = null,
+                Salary = 8000
+            };
+
+            var e7 = new Emp
+            {
+                Deptno = null,
+                Empno = 6,
+                Ename = "Andrzej Kwiatkowski",
+                HireDate = DateTime.Now.AddMonths(-3),
+                Job = "System administrator",
+                Mgr = null,
+                Salary = 7500
+            };
+
+            var e8 = new Emp
+            {
+                Deptno = 2,
+                Empno = 7,
+                Ename = "Marcin Polewski",
+                HireDate = DateTime.Now.AddMonths(-3),
+                Job = "Mobile developer",
+                Mgr = null,
+                Salary = 4000
+            };
+
+            var e9 = new Emp
+            {
+                Deptno = 2,
+                Empno = 8,
+                Ename = "Władysław Torzewski",
+                HireDate = DateTime.Now.AddMonths(-9),
+                Job = "CTO",
+                Mgr = null,
+                Salary = 12000
+            };
+
+            var e10 = new Emp
+            {
+                Deptno = 2,
+                Empno = 9,
+                Ename = "Andrzej Dalewski",
+                HireDate = DateTime.Now.AddMonths(-4),
+                Job = "Database administrator",
+                Mgr = null,
+                Salary = 9000
+            };
+
+            empsCol.Add(e1);
+            empsCol.Add(e2);
+            empsCol.Add(e3);
+            empsCol.Add(e4);
+            empsCol.Add(e5);
+            empsCol.Add(e6);
+            empsCol.Add(e7);
+            empsCol.Add(e8);
+            empsCol.Add(e9);
+            empsCol.Add(e10);
+            Emps = empsCol;
+
+            #endregion
+
+        }
+
+
+        /*
+            Celem ćwiczenia jest uzupełnienie poniższych metod.
+         *  Każda metoda powinna zawierać kod C#, który z pomocą LINQ'a będzie realizować
+         *  zapytania opisane za pomocą SQL'a.
+         *  Rezultat zapytania powinien zostać wyświetlony za pomocą kontrolki DataGrid.
+         *  W tym celu końcowy wynik należy rzutować do Listy (metoda ToList()).
+         *  Jeśli dane zapytanie zwraca pojedynczy wynik możemy je wyświetlić w kontrolce
+         *  TextBox WynikTextBox.
+        */
+
+        /// <summary>
+        /// SELECT * FROM Emps WHERE Job = "Backend programmer";
+        /// </summary>
+        public void Przyklad1()
+        {
+            /*
+            var res = new List<Emp>();
+            foreach(var emp in Emps)
+            {
+                if (emp.Job == "Backend programmer") res.Add(emp);
+            }
+            */
+
+            //1. Query syntax (SQL)
+            /*var res = from emp in Emps
+                      where emp.Job == "Backend programmer"
+                      select new
+                      {
+                          Nazwisko = emp.Ename,
+                          Zawod = emp.Job
+                      };
+                      */
+            string regex = "Backend programmer";
+            var result = Emps.Where(n => n.Job==regex);
+            foreach (var number in result)
+                Console.WriteLine(number);
+
+
+
+            //2. Lambda and Extension methods
+        }
+
+        /// <summary>
+        /// SELECT * FROM Emps Job = "Frontend programmer" AND Salary>1000 ORDER BY Ename DESC;
+        /// </summary>
+        public void Przyklad2()
+        {
+            string regex = "Frontend programmer";
+            var result = Emps.Where(n => n.Job == regex && n.Salary>1000);
+            result.OrderBy(n => n.Ename);
+            foreach (var number in result)
+                Console.WriteLine(number);
+        }
+
+        /// <summary>
+        /// SELECT MAX(Salary) FROM Emps;
+        /// </summary>
+        public void Przyklad3()
+        {
+            var result = Emps.Select(n => n.Salary);
+            var finalResult= result.Max();
+                Console.WriteLine(finalResult);
+        }
+
+        /// <summary>
+        /// SELECT * FROM Emps WHERE Salary=(SELECT MAX(Salary) FROM Emps);
+        /// </summary>
+        public void Przyklad4()
+        {
+            var result = Emps.Select(n => n.Salary);
+            var finalResult = result.Max();
+            var outer = Emps.Where(n => n.Salary == finalResult);
+            foreach (var number in outer)
+                Console.WriteLine(number);
+        }
+
+        /// <summary>
+        /// SELECT ename AS Nazwisko, job AS Praca FROM Emps;
+        /// </summary>
+        public void Przyklad5()
+        {
+            var result = Emps.Select(n => new
+            {
+                Praca = n.Job,
+                Nazwisko = n.Ename
+            });
+            foreach (var number in result)
+                Console.WriteLine(number);
+        }
+
+        /// <summary>
+        /// SELECT Emps.Ename, Emps.Job, Depts.Dname FROM Emps
+        /// INNER JOIN Depts ON Emps.Deptno=Depts.Deptno
+        /// Rezultat: Złączenie kolekcji Emps i Depts.
+        /// </summary>
+        public void Przyklad6()
+        { 
+                var res = Emps
+                .Join(Depts, emp => emp.Deptno, dept => dept.Deptno, (emp, dept) => new
+                {
+                    emp,
+                    dept
+                });
+            foreach (var number in res)
+                Console.WriteLine(number);
+        }
+
+        /// <summary>
+        /// SELECT Job AS Praca, COUNT(1) LiczbaPracownikow FROM Emps GROUP BY Job;
+        /// </summary>
+        public void Przyklad7()
+        {
+            var res = from emp in Emps
+                      group emp by emp.Job into jobGroup
+                      select new
+                      {
+                          Praca = jobGroup.Key,
+                          Licznosc = (from emp in Emps
+                                      where emp.Job == jobGroup.Key
+                                      select emp
+                              ).Count()
+                      };
+            res.ToList().ForEach(x => Console.WriteLine(x));
+        }
+
+        /// <summary>
+        /// Zwróć wartość "true" jeśli choć jeden
+        /// z elementów kolekcji pracuje jako "Backend programmer".
+        /// </summary>
+        public void Przyklad8()
+        {
+            var job = "Backend programmer";
+            bool hasJob = Emps.Any(emp => emp.Job == job);
+            Console.WriteLine($"Emp contains {job}: {hasJob}");
+        }
+
+        /// <summary>
+        /// SELECT TOP 1 * FROM Emp WHERE Job="Frontend programmer"
+        /// ORDER BY HireDate DESC;
+        /// </summary>
+        public void Przyklad9()
+        {
+            var res = from emp in Emps
+                      where emp.Job == "Frontend programmer"
+                      orderby emp.HireDate descending
+                      select new
+                      {
+                          emp.Empno,
+                          emp.Ename,
+                          emp.Job,
+                          emp.Salary,
+                          emp.HireDate,
+                          emp.Deptno,
+                          emp.Mgr
+                      };
+            Console.WriteLine(res.First());
+        }
+
+        /// <summary>
+        /// SELECT Ename, Job, Hiredate FROM Emps
+        /// UNION
+        /// SELECT "Brak wartości", null, null;
+        /// </summary>
+        public void Przyklad10Button_Click()
+        {
+            // nigdy tego union nie ogarnialem
+        }
+
+        //Znajdź pracownika z najwyższą pensją wykorzystując metodę Aggregate()
+        public void Przyklad11()
+        {
+            var result = Emps
+    .Select(emp => emp.Salary)
+    .Aggregate((res, next) => res > next ? res : next);
+            Console.WriteLine(result);
+        }
+
+        //Z pomocą języka LINQ i metody SelectMany wykonaj złączenie
+        //typu CROSS JOIN
+        public void Przyklad12()
+        {
+            var res = from emp in Emps
+                      from dept in Depts
+                      select new
+                      {
+                          emp.Empno,
+                          emp.Salary,
+                          dept.Deptno,
+                          dept.Dname,
+                          dept.Loc
+                      };
+            res.ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
---
+++ LinqConsoleApp/LinqConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            Console.WriteLine("Zadanie1");
+            d.Przyklad1();
+            Console.WriteLine("Zadanie 2");
+            d.Przyklad2();
+            Console.WriteLine("Zadanie 3");
+            d.Przyklad3();
+            Console.WriteLine("Zadanie 4");
+            d.Przyklad4();
+            Console.WriteLine("Zadanie 5");
+            d.Przyklad5();
+            Console.WriteLine("Zadanie 6");
+            d.Przyklad6();
+            Console.WriteLine("Zadanie 7");
+            d.Przyklad7();
+        }
+    }
+}
